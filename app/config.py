4b9a33dbf9efaa93e57b9aa978c8d586ea211601
szy <shizhenyu96@gmail.com>
2016-10-14 10:13:34 +0800

    10.14.10:13

--- app/config.py
+++ app/config.py
@@ -6,11 +6,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DB_HOSTNAME = 'localhost'
 DB_PORT = '3306'
-DB_USERNAME = 'szy'
-DB_PASSWORD = '123456'
+DB_USERNAME = 'root'
+DB_PASSWORD = 'hafjd867'
 DB_NAME = 'weixin'
-APP_ID = 'wx92a9c02f38'
-SECRET = 'aed361bef8658588'
+APP_ID = ''
+SECRET = ''
 TOKEN = ''
 
 
@@ -39,76 +39,21 @@ config = {
 MENU = {
     "button": [
         {
-
+            "type": "view",
             "name": "会议相关",
-            "sub_button": [
-                {
-                    "type": "click",
-                    "name": "未读消息",
-                    "key": "not_read_mes"
-                },
-                {
-                    "type": "view",
-                    "name": "历史消息",
-                    "url": "https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s"
-                           "&redirect_uri=%s"
-                           "/history&response_type=code&scope=snsapi_base&state=123&connect_redirect=1#wechat_redirect"
-                           % (APP_ID,ADDRESS)
-                }#,
-                # {
-                #     "type": "click",
-                #     "name": "近期消息",
-                #     "key": "recent_mes"
-                # }
-                ]
+            "url": "http://121.42.216.141"
 
         },
         {
+            "type": "view",
             "name": "赴会方式登记",
-            "sub_button": [
-                {
-                    "type": "click",
-                    "name": "日常考核",
-                    "key": "daily_assess"
-                },
-                {
-                    "type": "click",
-                    "name": "绩点查询",
-                    "key": "gpa"
-                },
-                {
-                    "type": "click",
-                    "name": "推免查询",
-                    "key": "recom"
-                },
-                {
-                    "type": "click",
-                    "name": "导师查询",
-                    "key": "tutor"
-                }
-            ]
+            "url": "http://121.42.216.141"
+
         },
         {
-            "name": "返程方式",
-            "sub_button": [
-                {
-                    "type": "view",
-                    "name": "微信问问",
-                    "url": "http://121.42.216.141"
-                },
-                {
-                    "type": "view",
-                    "name": "教务推送",
-                    "url": "http://121.42.216.141"
-                },
-                {
-                    "type": "scancode_push",
-                    "name": "扫码推事件",
-                    "key": "rselfmenu_0_1",
-                    "sub_button": []
-                }
-            ]
-
+            "type": "view",
+            "name": "返程方式登记",
+            "url": "http://121.42.216.141"
         }
 
     ]
--- app/menu.py
+++ app/menu.py
@@ -1,26 +1,23 @@
-# # -*- coding:utf8 -*-
-# # Author: shizhenyu96@gamil.com
-# # github: https://github.com/imndszy
-# import json
-# import requests
-# import logging
-#
-#
-# def create_menu():
-#     acc_token = get_token()
-#     if acc_token:
-#         url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s" % acc_token
-#         data = MENU
-#         request = requests.post(url, json.dumps(data, ensure_ascii=False))
-#         return request.text
-#     else:
-#         return "failed to get access_token!--menu.py"
-#
-#
-# if __name__ == "__main__":
-#     logging.basicConfig(level=logging.DEBUG,
-#                         format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
-#                         datefmt='%a, %d %b %Y %H:%M:%S',
-#                         filename='./log/menu.log',
-#                         filemode='w')
-#     logging.info('执行菜单更新操作' + str(create_menu()))
+# -*- coding:utf8 -*-
+# Author: shizhenyu96@gamil.com
+# github: https://github.com/imndszy
+import json
+import requests
+
+from app.get_acc_token import get_token
+from app.config import MENU
+
+
+def create_menu():
+    acc_token = get_token()
+    if acc_token:
+        url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s" % acc_token
+        data = MENU
+        request = requests.post(url, json.dumps(data, ensure_ascii=False))
+        return request.text
+    else:
+        return "failed to get access_token!--menu.py"
+
+
+if __name__ == "__main__":
+    print create_menu()
