a0d95a601cf210d13a22291b084c48ad46c481d3
szy <shizhenyu96@gmail.com>
2016-10-20 18:38:46 +0800

    amend function

--- app/handler.py
+++ app/handler.py
@@ -1,22 +1,22 @@
 # -*- coding:utf8 -*-
 # Author: shizhenyu96@gamil.com
 # github: https://github.com/imndszy
-from app.db import insert, create_engine, select, update
+from app.db import insert, select, update
 
 
 def handle_arrive_post(**kw):
-    result = select('select * from wechat1 where username=? and school=?', kw['username'], kw['school'])
+    result = select('select * from wechat where username=? and school=?', kw['username'], kw['school'])
     if result:
-        update('delete from wechat1 where username=? and school = ?',kw['username'], kw['school'])
+        update('delete from wechat where username=? and school = ?',kw['username'], kw['school'])
     return insert('wechat1',**kw)
 
-def handle_arrive_post_2(**kw):
-    result = select('select * from wechat2 where username=? and school=?', kw['username'], kw['school'])
-    if result:
-        update('delete from wechat2 where username=? and school = ?',kw['username'], kw['school'])
-    return insert('wechat2',**kw)
+# def handle_arrive_post_2(**kw):
+#     result = select('select * from wechat2 where username=? and school=?', kw['username'], kw['school'])
+#     if result:
+#         update('delete from wechat2 where username=? and school = ?',kw['username'], kw['school'])
+#     return insert('wechat2',**kw)
 
-
-def handle_arrive_get(username,school):
-    return select('select * from wechat1 where username=? and school=?',username,school)
+#
+# def handle_arrive_get(username,school):
+#     return select('select * from wechat1 where username=? and school=?',username,school)
 
