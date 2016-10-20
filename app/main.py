ae8f50cd5ae550c58f90a2e90d280cb70c16f9d1
szy <shizhenyu96@gmail.com>
2016-10-20 18:37:59 +0800

    version 2.3

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ import hashlib
 from flask import Flask, request, make_response, render_template, session, jsonify
 
 from app.config import TOKEN
-from app.handler import handle_arrive_post,handle_arrive_post_2,handle_arrive_get
+from app.handler import handle_arrive_post
 from app.db import create_engine
 from app.config import DB_HOSTNAME,DB_PORT,DB_NAME,DB_USERNAME,DB_PASSWORD
 
@@ -32,16 +32,21 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/introduce')
+def introduce():
+    return render_template('introduce.html')
+
+
 @app.route('/arrive')
 def arrive():
     session['arrived'] = 'arrived'
     return render_template('Arrive.html')
-
-
-@app.route('/leave')
-def leave():
-    session['left'] = 'left'
-    return render_template('Leave.html')
+#
+#
+# @app.route('/leave')
+# def leave():
+#     session['left'] = 'left'
+#     return render_template('Leave.html')
 
 
 @app.route('/arrive_post',methods=['POST'])
@@ -57,22 +62,26 @@ def arrive_post():
         session['arrive'] = da.get('arrive').encode('utf8')
         session['arrive_time'] = da.get('arrivetime').encode('utf8')
         session['order_num1'] = da.get('ordernum1').encode('utf8')
+        session['leave'] = da.get('leave').encode('utf8')
+        session['leavetime'] = da.get('leave').encode('utf8')
+        session['ordernum2'] = da.get('ordernum2').encode('utf8')
         session['visit'] = da.get('visit').encode('utf8')
-        session['finished1'] = 'finished'
+        session['finished'] = 'finished'
         session.permanent = True
 
         handle_arrive_post(school=session['school'],company=session['company'],
                     username=session['username'],work=session['work'],
                     tel=session['tel'],arrive=session['arrive'],
                     arrivetime=session['arrive_time'],ordernum1=session['order_num1'],
-                    visit=session['visit'])
+                    visit=session['visit'],leave=session['leave'],
+                           leavetime=session['leavetime'],ordernum2=session['ordernum2'])
         return jsonify(result='ok')
     return 'ok'
 
 
 @app.route('/arrive_get',methods=['GET'])
 def arrive_get():
-    if request.args.get('num2') == '2' and session.get('finished1','') == 'finished':
+    if request.args.get('num2') == '2' and session.get('finished','') == 'finished':
         # session.clear()
         # result = handle_arrive_get(session.get('username'),session.get('school'))[0]
         return jsonify(result='ok',
@@ -84,38 +93,41 @@ def arrive_get():
                        arrivetime=session['arrive_time'],
                        visit=int(session['visit']),
                        ordernum1=session['order_num1'],
-                       arrive=int(session['arrive']))
+                       arrive=int(session['arrive']),
+                       leavetime = session['leavetime'],
+                       ordernum2 = session['ordernum2'],
+                       leave = int(session['leave']))
     return 'ok'
 
-
-@app.route('/leave_post',methods=['POST'])
-def leave_post():
-    if session.get('left') == 'left':
-        da = request.values
-        session['school'] = da.get('school').encode('utf8')
-        session['username'] = da.get('username').encode('utf8')
-        session['leave'] = da.get('leave').encode('utf8')
-        session['leavetime'] = da.get('leave').encode('utf8')
-        session['ordernum2'] = da.get('ordernum2').encode('utf8')
-        session['finished2'] = 'finished'
-        session.permanent = True
-        handle_arrive_post_2(school=session['school'], username=session['username'],
-                                 leave=int(session['leave']), leavetime=session['leavetime'],
-                                 ordernum2=session['ordernum2'])
-        return jsonify(result='ok')
-    return 'ok'
-
-
-@app.route('/leave_get')
-def leave_get():
-    if request.args.get('num2') == '2' and session.get('finished2') == 'finished':
-        return jsonify(result='ok',
-                       username=session['username'],
-                       school=session['school'],
-                       leavetime=session['leavetime'],
-                       ordernum2=session['ordernum2'],
-                       leave=int(session['leave']))
-    return 'ok'
+#
+# @app.route('/leave_post',methods=['POST'])
+# def leave_post():
+#     if session.get('left') == 'left':
+#         da = request.values
+#         session['school'] = da.get('school').encode('utf8')
+#         session['username'] = da.get('username').encode('utf8')
+#         session['leave'] = da.get('leave').encode('utf8')
+#         session['leavetime'] = da.get('leave').encode('utf8')
+#         session['ordernum2'] = da.get('ordernum2').encode('utf8')
+#         session['finished2'] = 'finished'
+#         session.permanent = True
+#         handle_arrive_post_2(school=session['school'], username=session['username'],
+#                                  leave=int(session['leave']), leavetime=session['leavetime'],
+#                                  ordernum2=session['ordernum2'])
+#         return jsonify(result='ok')
+#     return 'ok'
+#
+#
+# @app.route('/leave_get')
+# def leave_get():
+#     if request.args.get('num2') == '2' and session.get('finished2') == 'finished':
+#         return jsonify(result='ok',
+#                        username=session['username'],
+#                        school=session['school'],
+#                        leavetime=session['leavetime'],
+#                        ordernum2=session['ordernum2'],
+#                        leave=int(session['leave']))
+#     return 'ok'
 
 
 def verification():
